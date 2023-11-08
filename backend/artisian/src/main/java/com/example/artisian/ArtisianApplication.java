2b794d6dfe3243877b6cc0eef482a3b62b34485a
Sankeerth Rani <sn501304@dal.ca>
2023-11-08 18:56:33 -0400

    added backend base template to the repo

---
+++ backend/artisian/src/main/java/com/example/artisian/ArtisianApplication.java
@@ -0,0 +1,13 @@
+package com.example.artisian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisianApplication.class, args);
+	}
+
+}
---
+++ backend/artisian/src/test/java/com/example/artisian/ArtisianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artisian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
