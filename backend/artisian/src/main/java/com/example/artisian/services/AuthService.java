9eb0f34643289e4c6c25b9aab07d5c0c2ce3e76f
Taranjot Singh <tr548284@dal.ca>
2023-11-16 10:34:03 -0400

    feat: ✨ added the AuthService

---
+++ backend/artisian/src/main/java/com/example/artisian/services/AuthService.java
@@ -0,0 +1,120 @@
+package com.example.artisian.services;
+
+import com.example.artisian.dto.LoginDTO;
+import com.example.artisian.dto.MessageResponseDTO;
+import com.example.artisian.dto.RegisterDTO;
+import com.example.artisian.dto.UserInfoResponseDTO;
+import com.example.artisian.entities.ERole;
+import com.example.artisian.entities.RoleEntity;
+import com.example.artisian.entities.UserEntity;
+import com.example.artisian.repositories.RoleRepository;
+import com.example.artisian.repositories.UserRepository;
+import com.example.artisian.security.jwt.JwtUtils;
+import com.example.artisian.security.services.UserDetailsImpl;
+import com.example.artisian.utils.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    public ResponseEntity<?> authenticateUser(LoginDTO loginDTO) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(new UserInfoResponseDTO(userDetails.getId(),
+                        userDetails.getUsername(),
+                        userDetails.getEmail(),
+                        roles));
+    }
+
+    public ResponseEntity<?> registerUser(RegisterDTO registerDTO, MultipartFile image) throws IOException {
+        if (userRepository.existsByUsername(registerDTO.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponseDTO("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(registerDTO.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponseDTO("Error: Email is already in use!"));
+        }
+
+        var compressImage =  ImageUtils.compressImage(image.getBytes());
+
+        String strRole = registerDTO.getRole();
+        RoleEntity userRole;
+        if (strRole == null) {
+            userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        } else {
+            switch (strRole) {
+                case "admin":
+                    userRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    break;
+                case "artisan":
+                    userRole = roleRepository.findByName(ERole.ROLE_ARTISAN)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    break;
+                case "user":
+                    userRole = roleRepository.findByName(ERole.ROLE_USER)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    break;
+                default:
+                    throw new RuntimeException("Error: Role is not found.");
+            }
+        }
+
+        UserEntity user = new UserEntity(registerDTO.getUsername(),
+                registerDTO.getEmail(),
+                encoder.encode(registerDTO.getPassword()),
+                userRole, compressImage);
+
+        userRepository.save(user);
+        return ResponseEntity.ok(new MessageResponseDTO("User registered successfully!"));
+    }
+
+    public ResponseEntity<?> logoutUser() {
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new MessageResponseDTO("You've been signed out!"));
+    }
+
+}
