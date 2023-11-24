600af50804b2266a06d41886b3fb39debc036b3b
vikramv <vk485591@dal.ca>
2023-11-24 00:54:37 -0400

    integrated DeleteProduct

--- frontend/src/Components/DeleteProductForm.js
+++ frontend/src/Components/DeleteProductForm.js
@@ -1,41 +1,55 @@
-// DeleteProductForm.js
 import React, { useState } from 'react';
 
-const DeleteProductForm = ({ products, onRemove }) => {
-  const [selectedProduct, setSelectedProduct] = useState('');
+import { url } from '../utils/ApiUrls';
 
-  const handleDelete = () => {
-    // Implement deletion logic here
-    if (selectedProduct) {
-      onRemove(selectedProduct);
-      // Clear the selected product after deletion
-      setSelectedProduct('');
-    } else {
-      // Handle the case where no product is selected
-      alert('Please select a product to delete');
+const DeleteProductForm = ({ onRemove }) => {
+  const [productId, setProductId] = useState('');
+  // const [message, alert] = useState('');
+
+  const handleDelete = async () => {
+    try {
+      if (productId) {
+        const response = await fetch(`${url}/api/products/delete/${productId}`, {
+          method: 'DELETE',
+        });
+
+        if (response.ok) {
+          // If the deletion is successful
+          onRemove(productId);
+          alert('Product deleted successfully');
+        } else if (response.status === 404) {
+          // If the product is not found
+          alert('Product not found');
+        } else {
+          // Handle other error cases
+          alert('Error deleting product');
+        }
+      } else {
+        // Handle the case where no product ID is entered
+        alert('Please enter a product ID');
+      }
+    } catch (error) {
+      console.error('Error deleting product:', error);
+      alert('Error deleting product');
     }
   };
 
   return (
     <div>
-      {/* <h3>Delete Product</h3> */}
+      <h3>Delete Product</h3>
       <label>
-        Select Product:
-        <select
-          value={selectedProduct}
-          onChange={(e) => setSelectedProduct(e.target.value)}
-        >
-          <option value="">Select a product</option>
-          {products.map((product) => (
-            <option key={product.id} value={product.id}>
-              {product.name}
-            </option>
-          ))}
-        </select>
+        Product ID:
+        <input
+          value={productId}
+          onChange={(e) => setProductId(e.target.value)}
+        />
       </label>
       <button onClick={handleDelete}>Delete Product</button>
+      {/* {message && <p>{message}</p>} */}
+
     </div>
   );
 };
 
 export default DeleteProductForm;
+
