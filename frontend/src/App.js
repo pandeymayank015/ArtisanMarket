ac58a2dfade69fdf17ffa25ca35178f5d51ff03d
Taranjot Singh <tr548284@dal.ca>
2023-11-26 12:23:14 -0400

    feat: ✨ stored the jwt-token using localstorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import Events from './components/Events';
+import EventCreation from './components/EventCreation';
 
 function App() {
   return (
@@ -14,8 +17,11 @@ function App() {
         <Navbar />
         <h1>Welcome to Artisan Market</h1>
         <Routes>
-          <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/event-creation" element={<EventCreation />} />
         </Routes>
         <Footer />
       </>
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,20 +1,35 @@
 // src/Login.js
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { url } from '../utils/ApiUrls'
+import { url } from '../utils/ApiUrls';
 
 const Login = () => {
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post(url +'/auth/login', { username, password });
-      console.log(response.data);
+      const response = await axios.post(url + '/auth/login', { username, password });
+      console.log(response);
+      if (response.status === 200) {
+        const { jwtToken, username, email } = response.data;
+  
+        // Store the JWT token in localStorage
+        localStorage.setItem('jwtToken', jwtToken);
+
+        // Store user details in localStorage
+        localStorage.setItem('currentUser', JSON.stringify({ username, email }));
+  
+        // Redirect to the dashboard with the username as a parameter
+        navigate('/dashboard', { state: { username } });
+      }
     } catch (error) {
       console.error('Error during login:', error);
     }
   };
+  
 
   return (
     <div>
