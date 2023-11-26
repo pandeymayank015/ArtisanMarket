0008f853ab78abbae46f8af62298e5cc6996bbf6
Mayank Madan <my905874@dal.ca>
2023-11-26 15:02:53 -0400

    Added blog view page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
 import UpdateProductRatingForm from './components/12_Rating_&_Review/UpdateProductRatingForm';
 import Wishlist from './components/15_User_Wishlists/Wishlist';
 import ArtisanStoreManagement from './components/3_Admin/ArtisanStoreManagement';
 import ProductApprovalForm from './components/3_Admin/ProductApprovalForm';
-// src/App.js
-// import React from 'react';
-import './App.css';
 import ArtisanProfile from './components/ArtisanProfile';
 import CommunityForum from './components/CommunityForum';
 import Footer from './components/Footer';
@@ -16,9 +14,9 @@ import Navbar from './components/Navbar';
 import Signup from './components/Signup';
 import UserProfile from './components/UserProfile';
 import ArtisanWorkshopProfile from './components/artisan-workshop-profile/ArtisanWorkshopProfile';
+import BlogDetails from './components/artisan-workshop-profile/BlogDetails';
 import UploadForm from './components/resource-center/UploadForm';
 import ResourceCenter from './components/resource-center/resource-center';
-
 const App = () => {
   return (
     <Router>
@@ -38,6 +36,7 @@ const App = () => {
           <Route path="/ProductApprovalForm" element={<ProductApprovalForm />} />
           <Route path="/UpdateProductRatingForm" element={<UpdateProductRatingForm />} />
           <Route path="/Wishlist" element={<Wishlist />} />
+          <Route path="/blog" element={<BlogDetails />} />
         </Routes>
         <Footer />
       </div>
@@ -46,24 +45,3 @@ const App = () => {
 }
 
 export default App;
-
-
-
-// import './App.css';
-// import React from 'react';
-// import { Routes, Route, Link  } from 'react-router-dom';
-// import ProductApprovalForm from './components/ProductApprovalForm';
-// import ArtisanStoreManagement from './components/ArtisanStoreManagement';
-
-// const App = () => {
-//   return (
-//       {/* <Routes>
-//         <Route path="/ArtisanStoreManagement" element={<ArtisanStoreManagement />} />
-//         <Route path="/ProductApprovalForm" element={<ProductApprovalForm />} />
-//       </Routes> */}
-//       <Link to="/ArtisanStoreManagement">Artisan Store Management</Link>
-//       <Link to="/ProductApprovalForm">Product Approval Form</Link>
-//   );
-// }
-
-// export default App;
