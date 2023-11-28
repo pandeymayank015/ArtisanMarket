1056d24e0a5ddc0d820fa5783f501ad8abd59b64
vikramv <vk485591@dal.ca>
2023-11-28 11:19:39 -0400

    changed fetch to axios, added Links for pages

--- frontend/src/components/3_Admin/ProductForm.js
+++ frontend/src/components/3_Admin/ProductForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { url } from '../../utils/ApiUrls'
+import axios from 'axios';
 
 
 const ProductForm = ({ onSubmit }) => {
@@ -7,12 +8,14 @@ const ProductForm = ({ onSubmit }) => {
   const [productDescription, setProductDescription] = useState('');
   const [productPrice, setProductPrice] = useState('');
   const [productCategory, setProductCategory] = useState('');
-  const [productImage, setProductImage] = useState('');
+  const [productImage, setProductImage] = useState(null);
   
   const handleImageChange = (e) => {
+    // console.log('Image selected:', e.target.files[0]);
     const selectedImage = e.target.files[0];
     setProductImage(selectedImage);
-};
+  };
+  
 
 const handleSubmit = async (e) => {
   e.preventDefault();
@@ -24,15 +27,20 @@ const handleSubmit = async (e) => {
   formData.append('price', productPrice);
   formData.append('category', productCategory);
   formData.append('image', productImage);
+  formData.append('user_id', '1');
+  // console.log("formData:", formData);
+  console.log([...formData.entries()]);
 
   try {
-    const response = await fetch(url + '/products', {
-      method: 'POST',
-      body: formData,
+    // Use Axios for the file upload
+    const response = await axios.post(url + '/products', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
     });
 
-    if (response.ok) {
-      const addedProduct = await response.json();
+    if (response.status === 200) {
+      const addedProduct = response.data;
       onSubmit(addedProduct);
 
       alert('Product added!');
@@ -42,13 +50,25 @@ const handleSubmit = async (e) => {
       setProductPrice('');
       setProductCategory('');
       setProductImage(null); // Clear the selected image
-
     } else {
       console.error('Failed to add product:', response.statusText);
     }
   } catch (error) {
     alert('Error adding product:', error);
     console.error('Error adding product:', error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error('Server Error Data:', error.response.data);
+      console.error('Server Error Status:', error.response.status);
+      console.error('Server Error Headers:', error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('No Response Received:', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error Message:', error.message);
+    }
   }
 };
 
