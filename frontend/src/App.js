c9cc6088e962fda9581d2c04c0e1b6b6082e7cb9
Taranjot Singh <tr548284@dal.ca>
2023-11-27 23:26:23 -0400

    feat: ✨ put routes under authguard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ import ArtisanWorkshopProfile from './components/artisan-workshop-profile/Artisa
 import BlogDetails from './components/artisan-workshop-profile/BlogDetails';
 import UploadForm from './components/resource-center/UploadForm';
 import ResourceCenter from './components/resource-center/resource-center';
+import AuthGuard from './components/AuthGuard'; // Import the AuthGuard component
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('jwtToken') !== null);
@@ -36,27 +37,62 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          {isAuthenticated ? (
-            <>
-              <Route path="/resource-center" element={<ResourceCenter />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/logout" element={<Logout />} />
-            </>
-          ) : (
-            <>
-              <Route path="/user-profile" element={<UserProfile />} />
-              <Route path="/artisan-profile" element={<ArtisanProfile />} />
-              <Route path="/community-forum" element={<CommunityForum />} />
-            </>
-          )}
-          <Route path="/event-creation" element={<EventCreation />} />
-          <Route path="/artisan/:username" element={<ArtisanWorkshopProfile />} />
-          <Route path="/upload/:username" element={<UploadForm />} />
-          <Route path="/ArtisanStoreManagement" element={<ArtisanStoreManagement />} />
-          <Route path="/ProductApprovalForm" element={<ProductApprovalForm />} />
-          <Route path="/UpdateProductRatingForm" element={<UpdateProductRatingForm />} />
-          <Route path="/Wishlist" element={<Wishlist />} />
-          <Route path="/blog" element={<BlogDetails />} />
+          <Route
+            path="/resource-center"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><ResourceCenter /></AuthGuard>}
+          />
+          <Route
+            path="/events"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><Events /></AuthGuard>}
+          />
+          <Route
+            path="/logout"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><Logout onLogout={handleLogout} /></AuthGuard>}
+          />
+          <Route
+            path="/user-profile"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><UserProfile /></AuthGuard>}
+          />
+          <Route
+            path="/artisan-profile"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><ArtisanProfile /></AuthGuard>}
+          />
+          <Route
+            path="/community-forum"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><CommunityForum /></AuthGuard>}
+          />
+          <Route
+            path="/event-creation"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><EventCreation /></AuthGuard>}
+          />
+          <Route
+            path="/artisan/:username"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><ArtisanWorkshopProfile /></AuthGuard>}
+          />
+          <Route
+            path="/upload/:username"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><UploadForm /></AuthGuard>}
+          />
+          <Route
+            path="/ArtisanStoreManagement"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><ArtisanStoreManagement /></AuthGuard>}
+          />
+          <Route
+            path="/ProductApprovalForm"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><ProductApprovalForm /></AuthGuard>}
+          />
+          <Route
+            path="/UpdateProductRatingForm"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><UpdateProductRatingForm /></AuthGuard>}
+          />
+          <Route
+            path="/Wishlist"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><Wishlist /></AuthGuard>}
+          />
+          <Route
+            path="/blog"
+            element={<AuthGuard isAuthenticated={isAuthenticated}><BlogDetails /></AuthGuard>}
+          />
         </Routes>
         <Footer />
       </div>
